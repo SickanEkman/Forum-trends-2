68bf592bac7a9124eb7f1e7982f4e4e4b467532e
Sara Ekman <sara.d.ekman@gmail.com>
2018-04-25 00:52:50 +0200

    csv file as database

--- prepare_og_files_weeks.py
+++ prepare_og_files_weeks.py
@@ -1,64 +1,50 @@
 import original_filenames
 import json
-import collections
-import os.path
+import os
 import datetime
-import pickle
 import tag_text
 import csv
-
+import conllu
 
 class PrepareOriginalFiles:
     def __init__(self):
         self.dir_with_og_files = "original_files/"
         for forum in original_filenames.forums:
-            self.prepare_files(forum)
+            self.prepare_file(forum)
 
-    def prepare_files(self, forum):
+    def prepare_file(self, forum):
+        udpipe_model = ""
         language = forum[-7:-5]
-        #if language == "sv":
-        #    udpipe_model = #todo: continue here!!!
-        path_to_file = os.path.join(self.dir_with_og_files, forum)
+        if language == "sv":
+            udpipe_model = os.path.join("udpipe_models", "swedish-ud-2.0-170801.udpipe")
+        if language == "en":
+            udpipe_model = os.path.join("udpipe_models", "english-ud-2.0-170801.udpipe")
+        path_to_og_file = os.path.join(self.dir_with_og_files, forum)
         forum_nick = forum[6]  # saves first letter in forum name
-        data = self.extract_data_of_interest(path_to_file)
-        #subdir_txt = forum_nick + "_weeks_readable"
-        self.store_time_periods_csv_files(data, forum_nick)
+        list_w_data_from_og_file = self.extract_data_from_og_file(path_to_og_file)
+        list_w_data_tuples = self.add_lemmas(list_w_data_from_og_file, udpipe_model)
+        self.store_time_periods_csv_files(list_w_data_tuples, forum_nick)
 
-    '''    if self.check_if_dir_exists(subdir_txt) == "Go ahead and create the files!":
-            self.store_time_periods_to_txt_files(data, forum_nick)
-        else:
-            pass
-        subdir_pkl = forum_nick + "_weeks_pickle"
-        if self.check_if_dir_exists(subdir_pkl) == "Go ahead and create the files!":
-            self.store_time_periods_to_pkl_file(data, forum_nick)
-        else:
-            pass
-    '''
-
-    def extract_data_of_interest(self, og_file):
+    def extract_data_from_og_file(self, og_file):
         """
         todo: update this
         """
-        data = collections.defaultdict(list)
-        data_list_w_tuples = []
+        data_list = []
         with open(og_file, "r") as fin:
             og_forum = json.load(fin)
         for dialogs in og_forum["dialogs"]:
             dialog_title = self.extract_text(dialogs, "title") + "."
             dialog_text = self.extract_text(dialogs, "content_text")
             dialog_date = self.extract_date(dialogs)
-            data_tuple = (dialog_date, "content_text", dialog_title, dialog_text)
-            data_list_w_tuples.append(data_tuple)
-            #data[dialog_date].append((dialog_title, dialog_text))
+            data_entry = [dialog_date, "content_text", dialog_title, dialog_text]
+            data_list.append(data_entry)
             for comments in dialogs["comments"]:
                 comment_text = self.extract_text(comments, "content_text")
                 comment_date = self.extract_date(comments)
-                data_tuple = (comment_date, "comments", dialog_title, comment_text)
-                data_list_w_tuples.append(data_tuple)
-                #data[comment_date].append((dialog_title, comment_text))
+                data_entry = [comment_date, "comments", dialog_title, comment_text]
+                data_list.append(data_entry)
         fin.close()
-        #return data
-        return data_list_w_tuples
+        return data_list
 
     def extract_text(self, text_field, part_to_extract):
         """
@@ -88,70 +74,61 @@ class PrepareOriginalFiles:
         the_date = date_yyyy + "-" + date_ww
         return the_date
 
-    def check_if_dir_exists(self, subdir):
-        try:
-            os.mkdir(subdir)
-            return "Go ahead and create the files!"
-        except FileExistsError:
-            print("Directory '%s' already exists." % subdir)
-            overwrite = input("Do you want to overwrite it? \ny/n\n")
-            if overwrite == "y" or overwrite == "Y":
-                return "Go ahead and create the files!"
-            else:
-                return "Directory already exist, don't overwrite!"
+    def add_lemmas(self, list_w_data_entries, udpipe_model):
+        list_w_tuples = []
+        model = tag_text.Model(udpipe_model)
+        for entry in list_w_data_entries:
+            title_tokenized = model.tokenize(entry[2])
+            title_lemmatized = self.tag_the_text(title_tokenized, model)
+            text_tokenized = model.tokenize(entry[3])
+            text_lemmatized = self.tag_the_text(text_tokenized, model)
+            tuple_w_data = (entry[0],
+                            entry[1],
+                            entry[2],
+                            title_lemmatized,
+                            entry[3],
+                            text_lemmatized,
+                            )
+            list_w_tuples.append(tuple_w_data)
+        return list_w_tuples
 
-    def store_time_periods_to_txt_files(self, data_dict, file_name):
-        """
-        todo: update this
-        """
-        subdir_txt = file_name + "_weeks_readable"
-        for date, texts in data_dict.items():
-            outfile_name = file_name + "_" + date + ".txt"
-            with open(os.path.join(subdir_txt, outfile_name), "w") as txt_fout:
-                for text in texts:
-                    txt_fout.write(text[0] + " ")
-                    txt_fout.write(text[1])
-            txt_fout.close()
-
-    def store_time_periods_to_pkl_file(self, data_dict, file_name):
-        subdir_pkl = file_name + "_weeks_pickle"
-        pkl_outfile_name = file_name + ".pkl"
-        with open(os.path.join(subdir_pkl, pkl_outfile_name), "wb") as pkl_fout:
-            pickle.dump(data_dict, pkl_fout)
-        pkl_fout.close()
+    def tag_the_text(self, text_chunk, model):
+        words_as_lemma = []
+        for sentence in text_chunk:
+            model.tag(sentence)
+        conllu_format = model.write(text_chunk, "conllu")
+        for sentence in conllu.parse(conllu_format):
+            for word in sentence:
+                if word["upostag"] != "PUNCT":
+                    lower_word = word["lemma"].lower()
+                    words_as_lemma.append(lower_word)
+        sentence_as_lemma = " ".join(words_as_lemma)
+        return sentence_as_lemma
 
     def store_time_periods_csv_files(self, data_list, forum_nick):
-        subdir_csv_files = forum_nick + "_weeks_csv"
-        if self.check_if_dir_exists(subdir_csv_files) == "Go ahead and create the files!":
-            csv_outfile_name = forum_nick + ".csv"
-            with open(os.path.join(subdir_csv_files, csv_outfile_name), "w", newline="") as csv_fout:
+        subdir_csv_files = "csv_files"
+        csv_outfile_name = forum_nick + ".csv"
+        full_path_to_file = os.path.join(subdir_csv_files, csv_outfile_name)
+        if self.check_if_file_exist_already(full_path_to_file) == "Go ahead and create the files!":
+            with open(full_path_to_file, "w", newline="") as csv_fout:
                 csv_writer = csv.writer(csv_fout, delimiter="\t", quotechar='"', quoting=csv.QUOTE_ALL)
-                csv_writer.writerow(["Week", "text type", "title", "text"])
+                csv_writer.writerow(["Week",
+                                     "text type",
+                                     "title",
+                                     "title_lemmatized",
+                                     "text",
+                                     "text_lemmatized",
+                                     ])
                 csv_writer.writerows(i for i in data_list)
             csv_fout.close()
 
-
-
-    def create_conllu_files(self):
-        file_list = []
-        model = tag_text.Model(training_file)
-        for dirpath, dirnames, filenames in os.walk(directory_name):
-            for filename in filenames:
-                if filename.endswith("txt"):
-                    file_list.append(os.path.join(dirpath, filename))
-            for f in file_list:
-                with open(f, "r") as fin:
-                    forum_text = fin.read()
-                    sentences = model.tokenize(forum_text)
-                    for s in sentences:
-                        model.tag(s)
-                    conllu = model.write(sentences, "conllu")
-
-                    print(conllu)
-                    go_on = input("continue?")
-
-                    outfile_name = f[9:18] + ".conllu"
-                    with open(os.path.join(dirpath, outfile_name), "w") as fout:
-                        fout.write(conllu)
-                    fout.close()
-                fin.close()
+    def check_if_file_exist_already(self, full_path_to_file):
+        if os.path.isfile(full_path_to_file) == False:
+            return "Go ahead and create the files!"
+        else:
+            print("File '%s' already exists." % full_path_to_file)
+        overwrite = input("Do you want to overwrite it? \ny/n\n")
+        if overwrite == "y" or overwrite == "Y":
+            return "Go ahead and create the files!"
+        else:
+            return "File already exist, don't overwrite!"
